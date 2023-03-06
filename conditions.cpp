c156cf622269ef10c18dfd2fdcce9e9fe18e7454
Alexey <chechetko-2003@mail.ru>
2023-03-06 22:54:33 +0300

    conditions

--- conditions.cpp
+++ conditions.cpp
@@ -1,13 +1,14 @@
-Conditions::Conditions(const vector<int> & v, const vector<int> & w, int K) {
-	m_w = w;
-	m_k = K;
+Conditions::Conditions(const vector<int>& v, const vector<int>& w, const int K) {
+	this -> v = v;
+	this -> w = w;
+	this -> K = K;
 }
 
-bool Conditions::is_good(Knapsack& knapsack) {
+/*bool Conditions::is_good(Knapsack& knapsack) {
 	int sum = 0;
 	for (int i = 0; i < m_v.size(); i++) {
 		sum += knapsack[i]*m_v[i];
 	}
 	return (sum < m_K);
 
-}
+}*/
--- conditions.h
+++ conditions.h
@@ -1,18 +1,15 @@
+#ifndef CONDITIONS_H_INCLUDED
+#define CONDITIONS_H_INCLUDED
+
 #include <vector>
-using namespace std;
-
-#include "knapsack.h" 
-
-#ifndef _conditions_h_
-#define _conditions_h_
 
 class Conditions {
 	public:
-		Conditions(const vector<int> & v, const vector<int> & w, int K) ;
-		bool is_good(Knapsack& knapsack); 
+		Conditions(const vector<int>& v, const vector<int>& w, const int K);
 	private:
-		vector<int> m_v;
-		vector<int> m_w;
-		int m_k;
+		vector<int> v;
+		vector<int> w;
+		int K;
 };
+
 #endif
