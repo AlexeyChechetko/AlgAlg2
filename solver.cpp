2740c6ab9d8bd6d4d1e171acefa886931dad3957
Alexey <chechetko-2003@mail.ru>
2023-03-20 16:42:46 +0300

    solver

--- solver.cpp
+++ solver.cpp
@@ -1,9 +1,19 @@
+#include <iostream>
 #include "solver.h"
 
 using namespace std;
 
 solver::solver(const vector<int>& c, const vector<int>& p, const int K_P, const int n) : C(c, p, K_P, n), K(n)
-	{}
+	{ answer.resize(n); this -> n = n; Max_c = 0; }
 
 void solver::solve()
 	{}
+
+void solver::print_ans()
+{
+	cout << Max_c << endl;
+	for (int i = 0; i < n; ++i)
+		cout << answer[i] << " ";
+	cout << endl;
+
+}
--- solver.h
+++ solver.h
@@ -11,9 +11,13 @@ class solver {
 	public:
 		solver(const vector<int>& c, const vector<int>& p, const int K_P, const int n);
 		void solve();
+		void print_ans();
 	private:
 		conditions C;
 		knapsack K;
+		vector<int> answer;
+		int n;
+		int Max_c;
 };
 
 #endif
